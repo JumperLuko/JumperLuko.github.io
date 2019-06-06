3deb17406295d2bb7732286b589796e54c955d99
Jumper Luko <jumper.luko@gmail.com>
2019-06-06 03:28:19 -0300

    Oh, great menu! Icons!!! Puts fiz uma puta seleção de icones e fiz diversos ajustes. E ainda tive que dar um jeito com a ancora do target!

---
+++ sobre/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<?php require_once('../rdd/head-upTree.html'); ?>
+	</head>
+	<body>
+		<?php require_once('../rdd/nav-upTree.html'); ?>
+		<main class="centerText h5">
+			<p>Adoro Web Design e pretendo transformar esse serviço em um meio de renda.</p>
+			<p>...</p>
+		</main>
+		<?php require_once('../rdd/footer-upTree.html'); ?>
+	</body>
+</html>
