633a9fe5b659b0db0c510e5133846109690d1c88
Jumper Luko <jumper.luko@gmail.com>
2019-06-30 15:05:17 -0300

    Corrigido bug do espaçamento nas img no celular.

--- portfolio/index.php
+++ portfolio/index.php
@@ -11,15 +11,15 @@
 				<p class="h4"><a href="http://marecianautica.com.br" target="_BLANK">Mar&Cia Náutica</a></p>
 				<p>Site da empresa de coletes Salva-Vidas, este é de rolagem continua com algumas animações interessantes, boa disposição de conteúdo, transições chamativas. E por curiosidade, um serviço a parte foram que quase todas as fotos foram batidas e editadas por mim.</p>
 				<p>O site é responsivo, ou seja, ele se ajusta a diferentes tamanhos de telas de maneira adequada. Na imagem a seguir você pode conferir como fica a visualização em um Galaxy S5, ou pode ir no site e ajustar o tamanho de sua janela!</p>
-				<a href="http://www.marecianautica.com.br" class="col-12 flex" target="_BLANK" title="Clique aqui para ir ao site da Mar&Cia em uma nova guia! /o/"><img src="../uploads/portifolio/marecia-site_S5.jpg" alt="Mar&Cia Náutica Site em Galaxy S5" style="margin:auto;"></a>
+				<a href="http://www.marecianautica.com.br" class="col-12 flex centerB" target="_BLANK" title="Clique aqui para ir ao site da Mar&Cia em uma nova guia! /o/"><img src="../uploads/portifolio/marecia-site_S5.jpg" alt="Mar&Cia Náutica Site em Galaxy S5"></a>
 			</div>
 			<div class="col-6" style="padding:2%;">
 				<p class="h4"><a href="http://snaketag.com.br/" target="_BLANK">SnakeTag</a></p>
 				<p>Esta é a empresa que eu criei para vender sites diretamente. Um site limpo e simplificado.</p>
 				<p>Abaixo visualização também em um galaxy S5!</p>
-				<a href="http://snaketag.com.br/" class="col-12 flex" target="_BLANK" title="Você passa o ponteiro em cima da imagem do Snaketag em um S5. Clique aqui para ir ao site da SnakeTag, tbm em uma nova guia! \o/"><img src="../uploads/portifolio/snaketag-site_S5.jpg" alt="SnakeTag Site em Galaxy S5" style="margin:auto;"></a>
+				<a href="http://snaketag.com.br/" class="col-12 flex centerB" target="_BLANK" title="Você passa o ponteiro em cima da imagem do Snaketag em um S5. Clique aqui para ir ao site da SnakeTag, tbm em uma nova guia! \o/"><img src="../uploads/portifolio/snaketag-site_S5.jpg" alt="SnakeTag Site em Galaxy S5"></a>
 				<p>Neste momento você pode ainda querer saber como fica a visualização em um Desktop, perceba algo similar em um Ipad.</p>
-				<a href="http://snaketag.com.br/" class="col-12 flex" target="_BLANK" title="Esta é a imagem do Ipad."><img src="../uploads/portifolio/snaketag-site_ipad.jpg" alt="SnakeTag Site em um Ipad" style="margin:auto;"></a>
+				<a href="http://snaketag.com.br/" class="col-12 flex centerB" target="_BLANK" title="Esta é a imagem do Ipad."><img src="../uploads/portifolio/snaketag-site_ipad.jpg" alt="SnakeTag Site em um Ipad"></a>
 			</div>
 			<div class="col-12 flex wrap">
 				<div>
@@ -29,17 +29,17 @@
 				</div>
 				<div class="col-6">
 					<p class="h4"><a href="https://www.youtube.com/channel/UCP5JaLctW__URGWk6yx7YVg" target="_BLANK">Arkantos</a></p>
-					<a href="../uploads/portifolio/Arkantos.svg" class="col-12 flex" target="_BLANK" title="Isologo Arkantos"><img src="../uploads/portifolio/Arkantos.svg" alt="Arkantos SVG" style="margin:auto;width:80%;"></a>
+					<a href="../uploads/portifolio/Arkantos.svg" class="col-12 flex centerB" target="_BLANK" title="Isologo Arkantos"><img src="../uploads/portifolio/Arkantos.svg" alt="Arkantos SVG" style="width:80%;"></a>
 					<p class="h4">Jumper</p>
-					<a href="../uploads/portifolio/JumperLuko.svg" class="col-12 flex" target="_BLANK" title="Isotipo Jumper"><img src="../uploads/portifolio/JumperLuko.svg" alt="JumperLuko SVG" style="margin:auto;width:80%;"></a>
+					<a href="../uploads/portifolio/JumperLuko.svg" class="col-12 flex centerB" target="_BLANK" title="Isotipo Jumper"><img src="../uploads/portifolio/JumperLuko.svg" alt="JumperLuko SVG" style="width:80%;"></a>
 					<p class="h4">SnakeTag</p>
-					<a href="../uploads/portifolio/SnakeTag.svg" class="col-12 flex" target="_BLANK" title="Imagotipo SnakeTag"><img src="../uploads/portifolio/SnakeTag.svg" alt="SnakeTag SVG" style="margin:auto;width:80%;"></a>
+					<a href="../uploads/portifolio/SnakeTag.svg" class="col-12 flex centerB" target="_BLANK" title="Imagotipo SnakeTag"><img src="../uploads/portifolio/SnakeTag.svg" alt="SnakeTag SVG" style="width:80%;"></a>
 				</div>
 				<div class="col-6">
 					<p class="h4">Nexware</p>
-					<a href="../uploads/portifolio/Nexware.svg" class="col-12 flex" target="_BLANK" title="Isotipo Nexware"><img src="../uploads/portifolio/Nexware.svg" alt="Nexware SVG" style="margin:auto;width:80%;"></a>
+					<a href="../uploads/portifolio/Nexware.svg" class="col-12 flex centerB" target="_BLANK" title="Isotipo Nexware"><img src="../uploads/portifolio/Nexware.svg" alt="Nexware SVG" style="width:80%;"></a>
 					<p class="h4">D¹2</p>
-					<a href="../uploads/portifolio/d¹2.svg" class="col-12 flex" target="_BLANK" title="Imagotipo d¹2"><img src="../uploads/portifolio/d¹2.svg" alt="Nexware SVG" style="margin:auto;width:80%;"></a>
+					<a href="../uploads/portifolio/d¹2.svg" class="col-12 flex centerB" target="_BLANK" title="Imagotipo d¹2"><img src="../uploads/portifolio/d¹2.svg" alt="Nexware SVG" style="width:80%;"></a>
 				</div>
 				</div>
 		</main>
