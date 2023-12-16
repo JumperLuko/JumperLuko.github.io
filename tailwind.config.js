e5ec9d1945627bedf772e0cd863d477ff0c47da0
JumperLuko <jumper.luko@gmail.com>
2023-12-16 23:25:44 +0000

    I make that shity dark mode swith with system theme work completely!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
-  theme: {
-    extend: {},
-  },
+  content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        animation: {
+            'spin-slow': 'spin 5s linear infinite',
+        },
+      },
+    },
   plugins: [],
 }
 
