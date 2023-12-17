9cddfcdd170fee83993c2b989b30beb6e1a29706
JumperLuko <jumper.luko@gmail.com>
2023-12-17 00:25:13 +0000

    Dark mode is perfect!!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  darkMode: 'class',
+  darkMode: 'class', //media or class
     theme: {
       extend: {
         colors: {
